430d260ced2154914b2f6c6f8de3d5d10acd67ee
morganherlocker <morgan.herlocker@gmail.com>
2014-10-01 11:26:58 -0400

    add triple test

--- test.js
+++ test.js
@@ -32,5 +32,50 @@ test('kinks', function(t){
 
   var hourglassKinks = kinks(hourglass)
   t.ok(hourglassKinks, 'get self intersection from hourglass polygon')
-  t.equal(hourglass.features.length, 1)
+  t.equal(hourglassKinks.features.length, 1)
+
+  var triple = {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -44.384765625,
+              1.0106897682409128
+            ],
+            [
+              -53.4375,
+              0.4833927027896987
+            ],
+            [
+              -43.154296875,
+              -6.402648405963884
+            ],
+            [
+              -53.173828125,
+              -6.708253968671543
+            ],
+            [
+              -43.857421875,
+              -13.752724664396975
+            ],
+            [
+              -54.09667968749999,
+              -14.944784875088372
+            ],
+            [
+              -53.3935546875,
+              -11.867350911459308
+            ],
+            [
+              -44.384765625,
+              1.0106897682409128
+            ]
+          ]
+        ]
+      }
+    }
+
+  var tripleKinks = kinks(triple)
+  t.ok(tripleKinks, 'get self intersection from hourglass polygon')
+  t.equal(tripleKinks.features.length, 3)
 })
