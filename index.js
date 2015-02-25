e392ea4eb6186882e94c319d18dce6641fd98c15
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-25 11:53:07 -0800

    Switch doc to closure compiler templating

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
- * Takes a {@link Polygon} feature and returns a {@link FeatureCollection} of {@link Point} features at all self-intersections.
+ * Takes a polygon and returns points at all self-intersections.
  *
  * @module turf/kinks
  * @category misc
- * @param {Polygon} polygon a Polygon feature
- * @returns {FeatureCollection} a FeatureCollection of {@link Point} features representing self-intersections
+ * @param {Feature<Polygon>} polygon input polygon
+ * @returns {FeatureCollection<Point>} self-intersections
  * @example
  * var poly = {
  *   "type": "Feature",
